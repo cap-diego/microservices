e6fca9bce083aeed9b09fdc3cb4f2bf24b3a875a
cap-diego <diegobuceta35@gmail.com>
2020-10-12 19:23:25 -0300

    Added gorilla/mux

--- handlers/products.go
+++ handlers/products.go
@@ -1,11 +1,12 @@
 package handlers
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/cap-diego/microservices/data"
 	"log"
-	"regexp"
 	"strconv"
 	"net/http"
+	"context"
 )
 
 // Products rest resource, implements ServeHTTP
@@ -13,49 +14,14 @@ type Products struct {
 	l *log.Logger
 }
 
+type KeyProduct struct{}
+
 // NewProducts rest resource for products
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-//ServeHTTP is the main entry point for the products. Satisfies http.Handler interface
-func (prods *Products) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		prods.getProducts(rw, req)
-		return
-	}
-
-	if req.Method == http.MethodPost {
-		prods.addProduct(rw, req)
-		return
-	}
-
-	if req.Method == http.MethodPut {
-		//Expects id in the url
-		reg := regexp.MustCompile(`/([0-9]+)`)
-		res := reg.FindAllStringSubmatch(req.URL.Path, -1)
-		if len(res) != 1{
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-		if len(res[0]) != 2 {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-		idString := res[0][1]
-		id, err := strconv.Atoi(idString)
-		if err != nil {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-		prods.updateProducts(id, rw, req)
-		return 
-	}
-
-	rw.WriteHeader(http.StatusMethodNotAllowed)
-}
-
-func (prods *Products) getProducts(rw http.ResponseWriter, h *http.Request) {
+func (prods *Products) GetProducts(rw http.ResponseWriter, h *http.Request) {
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
@@ -63,22 +29,22 @@ func (prods *Products) getProducts(rw http.ResponseWriter, h *http.Request) {
 	}
 }
 
-func (prods *Products) addProduct(rw http.ResponseWriter, req *http.Request) {
-	newProd := &data.Product{}
-	err := newProd.FromJSON(req.Body)
-	if err != nil {
-		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
-	}
-	data.AddProduct(newProd)
+func (prods *Products) AddProduct(rw http.ResponseWriter, req *http.Request) {
+	newProd := req.Context().Value(KeyProduct{}).(data.Product)
+	data.AddProduct(&newProd)
 }
 
-func (prods *Products) updateProducts(id int, rw http.ResponseWriter, req *http.Request) {
-	prod := &data.Product{}
-	err := prod.FromJSON(req.Body)
+func (prods *Products) UpdateProducts(rw http.ResponseWriter, req *http.Request) {
+	requestVars := mux.Vars(req)
+	id, err := strconv.Atoi(requestVars["id"])
 	if err != nil {
-		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+		http.Error(rw, "Unable to convert ID from url", http.StatusBadRequest)
+		return
 	}
-	err = data.UpdateProduct(id, prod)
+
+	prod := req.Context().Value(KeyProduct{}).(data.Product)
+
+	err = data.UpdateProduct(id, &prod)
 	if err == data.ErrProductNotFound {
 		http.Error(rw, "Product not found", http.StatusNotFound)
 		return
@@ -89,9 +55,34 @@ func (prods *Products) updateProducts(id int, rw http.ResponseWriter, req *http.
 	} 
 }
 
+func (prods Products) MiddlewareProductValidation(next http.Handler) http.Handler {
+	f := func(rw http.ResponseWriter, req *http.Request) {
+		prod := data.Product{}
+		err := prod.FromJSON(req.Body)
+		if err != nil {
+			prods.l.Println("[ERROR] desarializing product",err)
+			http.Error(rw, "Error reading product", http.StatusBadRequest)
+			return
+		}
+
+		// Add product to the context 
+		ctx := context.WithValue(req.Context(), KeyProduct{}, prod)
+		req = req.WithContext(ctx)
+		prods.l.Printf("CONTEXTO %#v", ctx)
+		// Call the next handler 
+		next.ServeHTTP(rw, req)
+	}
+	return http.HandlerFunc(f)
+}
+
+
+
+
+
 //With marshal
 // d, err := json.Marshal(lp)
 // if err != nil {
 // 	http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 // }
 // rw.Write(d)
+
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"net/http"
 	"log"
+	"github.com/gorilla/mux"
 	"github.com/cap-diego/microservices/handlers"
 	"os/signal"
 )
@@ -13,11 +14,20 @@ import (
 func main() {
 
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	// hh := handlers.NewHome(l)
 	ph := handlers.NewProducts(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()	
+	getRouter.HandleFunc("/products", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareProductValidation)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareProductValidation)
 
 	server := &http.Server{
 		Addr: ":9090",
